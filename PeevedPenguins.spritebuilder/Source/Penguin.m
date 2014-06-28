929103e8ac9201fe375363de87850eec972d75dc
Nicholas Kee <nicholaskee@yayas-mac-mini.gateway.2wire.net>
2014-06-28 16:22:37 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -0,0 +1,15 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Nicholas Kee on 6/28/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,15 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Nicholas Kee on 6/28/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+
+
+@end
