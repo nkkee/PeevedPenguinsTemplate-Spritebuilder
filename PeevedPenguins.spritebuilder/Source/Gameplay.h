81a1d51045b48cc754515a2994f8013cf76d38a4
Nicholas Kee <nicholaskee@yayas-mac-mini.gateway.2wire.net>
2014-06-29 06:23:48 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,16 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Nicholas Kee on 6/28/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Gameplay : CCSprite
+
+- (void) touchMoved:(UITouch *)touch withEvent:(UIEvent *)event;
+
+
+@end
